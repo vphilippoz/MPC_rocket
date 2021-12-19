e7b16c97f5339505b60f31873105462ee3ea38f7
Francesco217 <francesco.nonis@gmail.com>
2021-12-19 11:36:40 +0100

    update 3.2

---
+++ Deliverable_3_2/MPC_Control_y.m
@@ -0,0 +1,131 @@
+classdef MPC_Control_y < MPC_Control
+    
+    methods
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc, Ts, H)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   X(:,1)       - initial state (estimate)
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   U(:,1)       - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            N = ceil(H/Ts); % Horizon steps
+
+            [nx, nu] = size(mpc.B);
+            
+            % Targets (Ignore this before Todo 3.2)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+            
+            % Predicted state and input trajectories
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
+            %       the DISCRETE-TIME MODEL of your system
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+            
+            
+            % Cost matrices 
+            Q = eye(nx);
+            R = eye(nu);
+            
+            % Terminal cost  as LQR cost
+            [K, Qf, ~] = dlqr(mpc.A, mpc.B, Q, R);
+            K = -K
+            
+            %find the steady states inputs
+   
+            target_opti = mpc.setup_steady_state_target()
+         
+            
+            
+            % Constraints
+            %   State constraints
+            F = [0 1 0 0;...
+                 0 -1 0 0];
+            f = [deg2rad(5);deg2rad(5)];
+            
+            %  Input contstraints
+            M = [1; -1];
+            m = [deg2rad(15); deg2rad(15)];
+            
+            % Maximal invariant set
+            Xf = polytope([F;M*K],[f;m]);
+            
+            % Terminal set
+            Acl = [mpc.A + mpc.B*K];
+            while 1
+                prevXf = Xf;
+                [T,t] = double(Xf);
+                preXf = polytope(T*Acl,t);
+                Xf = intersect(Xf, preXf);
+                if isequal(prevXf, Xf)
+                    break
+                end
+            end
+            [Ff,ff] = double(Xf);
+
+            % Constraints and objective
+            con = (X(:,2) == mpc.A*X(:,1) + mpc.B*U(:,1)) + (M*U(:,1) <= m);
+            obj = U(:,1)'*R*U(:,1);
+            for i = 2:N-1
+                con = con + (X(:,i+1) == mpc.A*X(:,i) + mpc.B*U(:,i));
+                con = con + (F*X(:,i) <= f) + (M*U(:,i) <= m);
+                obj = obj + X(:,i)'*Q*X(:,i) + U(:,i)'*R*U(:,i);
+            end
+            %   Terminal constraints and objective
+            con = con + (Ff*X(:,N) <= ff);
+            obj = obj + X(:,N)'*Qf*X(:,N);
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {X(:,1), x_ref, u_ref}, U(:,1));
+        end
+        
+        % Design a YALMIP optimizer object that takes a position reference
+        % and returns a feasible steady-state state and input (xs, us)
+        function target_opti = setup_steady_state_target(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            % OUTPUTS
+            %   xs, us - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            nx = size(mpc.A, 1);
+
+            % Steady-state targets
+            xs = sdpvar(nx, 1);
+            us = sdpvar;
+            
+            % Reference position (Ignore this before Todo 3.2)
+            ref = sdpvar;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+            Rs = 1;
+            obj = us'*Rs*us;
+            con = [xs == mpc.A*xs + mpc.B*us, mpc.C*xs == ref];
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute the steady-state target
+            target_opti = optimizer(con, obj, sdpsettings('solver', 'gurobi'), ref, {xs, us});
+        end
+    end
+end
---
+++ todo32.m
@@ -0,0 +1,49 @@
+%% TODO 3.2
+clc; clear; close all;
+addpath(fullfile('src'));
+addpath(fullfile('Deliverable_3_2'));
+
+
+% Initial pose (in SI units)
+x0 = [0, 0, 0, 5].'; % for system x: ωy , β, vx , x
+y0 = [0, 0, 0, 5].'; % for system y: ωx , α, vy , y
+z0 = [0, 5].'; % for system z: vz, z
+roll0 = [0.2, deg2rad(2)].'; % for system roll: ωz, γ
+
+% Create rocket
+Ts = 1/20; % Sample time
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+
+% Design MPC controller
+H = 5; % Horizon length in seconds
+mpc_x = MPC_Control_x(sys_x, Ts, H);
+mpc_y = MPC_Control_y(sys_y, Ts, H);
+mpc_z = MPC_Control_z(sys_z, Ts, H);
+mpc_roll = MPC_Control_roll(sys_roll, Ts, H);
+% Get control input
+ux = mpc_x.get_u(x0)
+uy = mpc_y.get_u(y0)
+uz = mpc_z.get_u(z0)
+uroll = mpc_roll.get_u(roll0)
+
+% Simulate
+Tf = 10; % [s]
+
+% %   x
+% [T, X_sub, U_sub] = rocket.simulate(sys_x, x0, Tf, @mpc_x.get_u, 0);
+% ph = rocket.plotvis_sub(T, X_sub, U_sub, sys_x, xs, us);
+
+%   y
+ [T, X_sub, U_sub] = rocket.simulate(sys_y, y0, Tf, @mpc_y.get_u, 0);
+ ph = rocket.plotvis_sub(T, X_sub, U_sub, sys_y, xs, us);
+
+% %   z
+% [T, X_sub, U_sub] = rocket.simulate(sys_z, z0, Tf, @mpc_z.get_u, 0);
+% ph = rocket.plotvis_sub(T, X_sub, U_sub, sys_z, xs, us);
+
+% %   roll
+% [T, X_sub, U_sub] = rocket.simulate(sys_roll, roll0, Tf, @mpc_roll.get_u, 0);
+% ph = rocket.plotvis_sub(T, X_sub, U_sub, sys_roll, xs, us);
