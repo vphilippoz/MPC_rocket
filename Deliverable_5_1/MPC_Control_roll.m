0e4dc20dbbaf375affcf311052463e4318b1b9f9
Francesco217 <francesco.nonis@gmail.com>
2021-12-20 18:12:23 +0100

    estimateur a tune

---
+++ Deliverable_5_1/MPC_Control_roll.m
@@ -0,0 +1,99 @@
+classdef MPC_Control_roll < MPC_Control
+    
+    methods
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc, Ts, H)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   X(:,1)       - initial state (estimate)
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   U(:,1)       - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            N = ceil(H/Ts); % Horizon steps
+            
+            [nx, nu] = size(mpc.B);
+            
+            % Steady-state targets (Ignore this before Todo 3.2)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+            
+            % Predicted state and input trajectories
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
+            %       the DISCRETE-TIME MODEL of your system
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+            
+            
+            % Cost matrices 
+            Q = 40*eye(nx);
+            R = 0.5*eye(nu);
+    
+            % Constraints
+            %   State constraints
+            % no state constraints
+            
+            %  Input contstraints
+            M = [1; -1];
+            m = [20; 20];
+            
+            % Constraints and objective
+            con = ((X(:,2)-x_ref) == mpc.A*(X(:,1)-x_ref) + mpc.B*(U(:,1)-u_ref)) + (M*(U(:,1)-u_ref) <= m);
+            obj = (U(:,1)-u_ref)'*R*(U(:,1)-u_ref);
+            for i = 2:N-1
+                con = con + ((X(:,i+1)-x_ref) == mpc.A*(X(:,i)-x_ref) + mpc.B*(U(:,i)-u_ref));
+                con = con + (M*(U(:,i)-u_ref) <= m);
+                obj = obj + (X(:,i)-x_ref)'*Q*(X(:,i)-x_ref) + (U(:,i)-u_ref)'*R*(U(:,i)-u_ref);
+            end
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {X(:,1), x_ref, u_ref}, U(:,1));
+        end
+        
+        % Design a YALMIP optimizer object that takes a position reference
+        % and returns a feasible steady-state state and input (xs, us)
+        function target_opti = setup_steady_state_target(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            % OUTPUTS
+            %   xs, us - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Steady-state targets
+            nx = size(mpc.A, 1);
+            xs = sdpvar(nx, 1);
+            us = sdpvar;
+            
+            % Reference position (Ignore this before Todo 3.2)
+            ref = sdpvar;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+            Rs = 1;
+            obj = us'*Rs*us;
+            con = [xs == mpc.A*xs + mpc.B*us, mpc.C*xs == ref];
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute the steady-state target
+            target_opti = optimizer(con, obj, sdpsettings('solver', 'gurobi'), ref, {xs, us});
+        end
+    end
+end
